8ce7be792f9c06479e0524b005a2b39d520b17f3
cjn <cjn@chaoweiinfo.com>
2016-06-30 18:37:01 +0800

    6.30

--- app/src/main/java/com/yrkj/yrlife/been/URLs.java
+++ app/src/main/java/com/yrkj/yrlife/been/URLs.java
@@ -13,9 +13,9 @@ public class URLs extends Entity {
 	public static  String secret_code="";
 //	public static String ImageByte="";
 	public static String IMAGE_FILE_NAME="";
-	public static final String HOST = "http://192.168.3.2:8080/";
+//	public static final String HOST = "http://192.168.3.2:8080/";
 //	public static final String HOST = "http://40057179.nat123.net/";
-//	public static final String HOST = "http://119.10.48.101/";
+	public static final String HOST = "http://119.10.48.101/";
 
 	public static final String APP_INIT = HOST+"wmmanager/app/pro1/app1/addMember";		//应用初始化地址
 	public static final String HOME_ADS = HOST+"wmmanager/app/pro1/app1/loadBannerImage" ;		//首页广告地址
